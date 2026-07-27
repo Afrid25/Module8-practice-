030655afb06a93c2ab5deee8e7c74879c2326314
Afrid25 <25afridfoisal00@gmail.com>
2026-07-27 23:52:30 +0600

    task one is complete and task 2 start

--- task1.js
+++ task1.js
@@ -1,2 +1,30 @@
 // Take two variables price1 and price2, and compare them using all comparison operators (==, ===, !=, !==, >, <, >=, <=), printing each result with console.log.
 
+let price1 = 500;
+price2 = 600;
+
+price1 > price2 ? console.log('Price one is high '): console.log('Price two is high ') ;
+
+console.log();
+
+price1 == price2 ? console.log('All price are same  '): console.log('All price are not same  ') ;
+console.log();
+
+price1 = 400;
+price2 = "400";
+
+price1 !== price2 ? console.log('All price are same  '): console.log('All price are not same  ') ;
+console.log();
+
+
+price1 = 400;
+price2 = "400";
+price1 !== price2 ? console.log('Both are different   '): console.log('All price are same  ') ;
+console.log();
+
+
+price1 = 800;
+price2 = 900;
+
+price1 >= price2 ? console.log('price one is largest  '): console.log(' price two is largest   ') ;
+
